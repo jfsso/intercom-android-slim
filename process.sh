be2f83bdd353c9309e08e4282ec3cca6d5e24c3f
João Orui <joao@joao.jp>
2015-10-02 13:54:41 +0900

    Generate slim AAR for version 1.1.7

--- process.sh
+++ process.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
+# cleanup
+rm -f ./intercom-sdk-base-1.1.7.aar
+rm -f ./intercom-sdk-base-1.1.7-slim.aar
+rm -rf ./intercom-sdk-base
+rm -rf ./temp
+
 # get intercom
-curl -o ./intercom-sdk-base-1.1.6.aar 'https://raw.githubusercontent.com/intercom/intercom-android/1.1.6/aar/intercom-sdk-base-1.1.6.aar'
+curl -o ./intercom-sdk-base-1.1.7.aar 'https://raw.githubusercontent.com/intercom/intercom-android/1.1.7/aar/intercom-sdk-base/intercom-sdk-base-1.1.7.aar'
 
 # get jarjar
 curl -o ./jarjar-1.4.jar 'https://jarjar.googlecode.com/files/jarjar-1.4.jar'
 
-# cleanup
-rm -f ./intercom-sdk-base-1.1.6-processed.aar
-rm -rf ./intercom-sdk-base
-rm -rf ./temp
-
 # process
-unzip ./intercom-sdk-base-1.1.6.aar -d ./intercom-sdk-base
+unzip ./intercom-sdk-base-1.1.7.aar -d ./intercom-sdk-base
 mkdir ./temp
 cp ./intercom-sdk-base/classes.jar ./temp/
 cp ./intercom-sdk-base/libs/repackaged_dependencies.jar ./temp/
@@ -24,5 +25,5 @@ mv ./temp/classes.jar ./intercom-sdk-base/classes.jar
 mv ./temp/repackaged_dependencies_2.jar ./intercom-sdk-base/libs/repackaged_dependencies.jar
 
 cd ./intercom-sdk-base
-zip -r -X ../intercom-sdk-base-1.1.6-processed.aar *
+zip -r -X ../intercom-sdk-base-1.1.7-slim.aar *
 cd ..
