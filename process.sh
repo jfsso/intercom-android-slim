f8d27c6f98269ae2d093ebe4137b730668e397ce
João Orui <joao@joao.jp>
2015-12-01 09:17:37 +0900

    Version 1.1.10

--- process.sh
+++ process.sh
@@ -11,7 +11,7 @@ mkdir -p $temp_dir
 mkdir -p $tools_dir
 
 # get intercom
-curl -o $temp_dir/intercom-sdk-base-1.1.9.aar 'https://raw.githubusercontent.com/intercom/intercom-android/1.1.9/aar/intercom-sdk-base/intercom-sdk-base-1.1.9.aar'
+curl -o $temp_dir/intercom-sdk-base-1.1.10.aar 'https://raw.githubusercontent.com/intercom/intercom-android/1.1.10/aar/intercom-sdk-base/intercom-sdk-base-1.1.10.aar'
 
 # get jarjar
 if [ ! -f $tools_dir/jarjar-1.4.jar ]; then
@@ -19,7 +19,7 @@ if [ ! -f $tools_dir/jarjar-1.4.jar ]; then
 fi
 
 # process
-unzip $temp_dir/intercom-sdk-base-1.1.9.aar -d $temp_dir/intercom-sdk-base
+unzip $temp_dir/intercom-sdk-base-1.1.10.aar -d $temp_dir/intercom-sdk-base
 cp $temp_dir/intercom-sdk-base/classes.jar $temp_dir
 cp $temp_dir/intercom-sdk-base/libs/repackaged_dependencies.jar $temp_dir
 java -Dverbose=true -jar $tools_dir/jarjar-1.4.jar process rules_classes.txt $temp_dir/intercom-sdk-base/classes.jar $temp_dir/classes.jar
@@ -30,7 +30,7 @@ mv $temp_dir/classes.jar $temp_dir/intercom-sdk-base/classes.jar
 mv $temp_dir/repackaged_dependencies_2.jar $temp_dir/intercom-sdk-base/libs/repackaged_dependencies.jar
 
 cd $temp_dir/intercom-sdk-base
-zip -r -X $output_dir/intercom-sdk-base-1.1.9-slim.aar *
+zip -r -X $output_dir/intercom-sdk-base-1.1.10-slim.aar *
 cd $work_dir
 
 rm -rf ./temp
