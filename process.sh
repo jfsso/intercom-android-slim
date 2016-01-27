78e7919903c8c8791ea56ca074b9465f33b60063
João Orui <joao@joao.jp>
2016-01-27 17:38:31 +0900

    Generate slim AAR for gcm module

--- process.sh
+++ process.sh
@@ -12,13 +12,15 @@ mkdir -p $tools_dir
 
 # get intercom
 curl -o $temp_dir/intercom-sdk-base-1.1.13.aar 'https://raw.githubusercontent.com/intercom/intercom-android/1.1.13/aar/intercom-sdk-base/intercom-sdk-base-1.1.13.aar'
+curl -o $temp_dir/intercom-sdk-gcm-1.1.13.aar 'https://raw.githubusercontent.com/intercom/intercom-android/1.1.13/aar/intercom-sdk-gcm/intercom-sdk-gcm-1.1.13.aar'
 
 # get jarjar
 if [ ! -f $tools_dir/jarjar-1.4.jar ]; then
   curl -o $tools_dir/jarjar-1.4.jar 'https://jarjar.googlecode.com/files/jarjar-1.4.jar'
 fi
 
-# process
+# process base
+cd $work_dir
 unzip $temp_dir/intercom-sdk-base-1.1.13.aar -d $temp_dir/intercom-sdk-base
 cp $temp_dir/intercom-sdk-base/classes.jar $temp_dir
 cp $temp_dir/intercom-sdk-base/libs/repackaged_dependencies.jar $temp_dir
@@ -31,6 +33,19 @@ mv $temp_dir/repackaged_dependencies_2.jar $temp_dir/intercom-sdk-base/libs/repa
 
 cd $temp_dir/intercom-sdk-base
 zip -r -X $output_dir/intercom-sdk-base-1.1.13-slim.aar *
+
+# process gcm
+cd $work_dir
+unzip $temp_dir/intercom-sdk-gcm-1.1.13.aar -d $temp_dir/intercom-sdk-gcm
+cp $temp_dir/intercom-sdk-gcm/classes.jar $temp_dir
+java -Dverbose=true -jar $tools_dir/jarjar-1.4.jar process rules_classes.txt $temp_dir/intercom-sdk-gcm/classes.jar $temp_dir/classes.jar
+
+mv $temp_dir/classes.jar $temp_dir/intercom-sdk-gcm/classes.jar
+
+cd $temp_dir/intercom-sdk-gcm
+zip -r -X $output_dir/intercom-sdk-gcm-1.1.13-slim.aar *
+
+# clean up
 cd $work_dir
 
-rm -rf ./temp
+rm -rf $temp_dir
