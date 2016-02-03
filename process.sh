47dacf23aff579cd00e5cf7a93bb2866382d364c
João Orui <joao@joao.jp>
2016-02-03 17:40:55 +0900

    Version 1.1.14

--- process.sh
+++ process.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+version="1.1.14"
 work_dir=`pwd`
 output_dir="$work_dir/output"
 temp_dir="$work_dir/temp"
@@ -11,8 +12,8 @@ mkdir -p $temp_dir
 mkdir -p $tools_dir
 
 # get intercom
-curl -o $temp_dir/intercom-sdk-base-1.1.13.aar 'https://raw.githubusercontent.com/intercom/intercom-android/1.1.13/aar/intercom-sdk-base/intercom-sdk-base-1.1.13.aar'
-curl -o $temp_dir/intercom-sdk-gcm-1.1.13.aar 'https://raw.githubusercontent.com/intercom/intercom-android/1.1.13/aar/intercom-sdk-gcm/intercom-sdk-gcm-1.1.13.aar'
+curl -o $temp_dir/intercom-sdk-base-$version.aar "https://raw.githubusercontent.com/intercom/intercom-android/$version/aar/intercom-sdk-base/intercom-sdk-base-$version.aar"
+curl -o $temp_dir/intercom-sdk-gcm-$version.aar "https://raw.githubusercontent.com/intercom/intercom-android/$version/aar/intercom-sdk-gcm/intercom-sdk-gcm-$version.aar"
 
 # get jarjar
 if [ ! -f $tools_dir/jarjar-1.4.jar ]; then
@@ -21,7 +22,7 @@ fi
 
 # process base
 cd $work_dir
-unzip $temp_dir/intercom-sdk-base-1.1.13.aar -d $temp_dir/intercom-sdk-base
+unzip $temp_dir/intercom-sdk-base-$version.aar -d $temp_dir/intercom-sdk-base
 cp $temp_dir/intercom-sdk-base/classes.jar $temp_dir
 cp $temp_dir/intercom-sdk-base/libs/repackaged_dependencies.jar $temp_dir
 java -Dverbose=true -jar $tools_dir/jarjar-1.4.jar process rules_classes.txt $temp_dir/intercom-sdk-base/classes.jar $temp_dir/classes.jar
@@ -32,18 +33,18 @@ mv $temp_dir/classes.jar $temp_dir/intercom-sdk-base/classes.jar
 mv $temp_dir/repackaged_dependencies_2.jar $temp_dir/intercom-sdk-base/libs/repackaged_dependencies.jar
 
 cd $temp_dir/intercom-sdk-base
-zip -r -X $output_dir/intercom-sdk-base-1.1.13-slim.aar *
+zip -r -X $output_dir/intercom-sdk-base-$version-slim.aar *
 
 # process gcm
 cd $work_dir
-unzip $temp_dir/intercom-sdk-gcm-1.1.13.aar -d $temp_dir/intercom-sdk-gcm
+unzip $temp_dir/intercom-sdk-gcm-$version.aar -d $temp_dir/intercom-sdk-gcm
 cp $temp_dir/intercom-sdk-gcm/classes.jar $temp_dir
 java -Dverbose=true -jar $tools_dir/jarjar-1.4.jar process rules_classes.txt $temp_dir/intercom-sdk-gcm/classes.jar $temp_dir/classes.jar
 
 mv $temp_dir/classes.jar $temp_dir/intercom-sdk-gcm/classes.jar
 
 cd $temp_dir/intercom-sdk-gcm
-zip -r -X $output_dir/intercom-sdk-gcm-1.1.13-slim.aar *
+zip -r -X $output_dir/intercom-sdk-gcm-$version-slim.aar *
 
 # clean up
 cd $work_dir
